acfa880c88a00e679e8b4cf686676890d17dbd51
petstyle-inc <153817510+petstyle-inc@users.noreply.github.com>
2024-04-03 18:31:06 +0900

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,31 +1,18 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-mb_language("Japanese");
-mb_internal_encoding("UTF-8");
+    $to = 'akagi01freestyle@gmail.com';
+    $subject = '新しいお問い合わせ';
+    $body = "名前: {$name}\nメールアドレス: {$email}\nメッセージ: {$message}";
+    $headers = "From: {$email}";
 
-if($_POST) {
-  $to = 'akagi01freestyle@gmail.com';
-  $subject = 'お問い合わせがありました';
-  
-  $message = "お問い合わせがありました。\n";
-  $message .= "\n";
-  $message .= "入力された内容は以下の通りです。\n";
-  $message .= "---\n";
-  $message .= "\n";
-  $message .= "お名前：\n";
-  $message .= $_POST["name"];
-  $message .= "\n";
-  $message .= "メールアドレス:\n";
-  $message .= $_POST["email"];
-  $message .= "\n";
-  $message .= "お問い合わせ本文:\n";
-  $message .= $_POST["message"];
-
-  if(mb_send_mail($to,$subject,$message)) {
-    echo "メールが送信されました";
-  } else {
-    echo "メールの送信に失敗しました";
-  }
-} else {
-  echo "HTMLからのPOST送信受信に失敗しました";
+    if (mail($to, $subject, $body, $headers)) {
+        echo 'お問い合わせありがとうございます。';
+    } else {
+        echo 'メールの送信に失敗しました。';
+    }
 }
+?>
