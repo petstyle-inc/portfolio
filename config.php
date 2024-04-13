a338244ec2f173511ef4244c719e2045bce07b81
petstyle-inc <153817510+petstyle-inc@users.noreply.github.com>
2024-04-13 12:46:46 +0900

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$settings = array(
+    'email' => 'akagi01freestyle@gmail.com',
+    // 他の設定値もここに追加できます
+);
---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php
+
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+include 'config.php';
+
+if($_POST) {
+  $to = $settings['email'];
+  $subject = 'お問い合わせがありました';
+  
+  $message = "お問い合わせがありました。\n";
+  $message .= "\n";
+  $message .= "入力された内容は以下の通りです。\n";
+  $message .= "---\n";
+  $message .= "\n";
+  $message .= "お名前：\n";
+  $message .= $_POST["name"];
+  $message .= "\n";
+  $message .= "メールアドレス:\n";
+  $message .= $_POST["email"];
+  $message .= "\n";
+  $message .= "お問い合わせ本文:\n";
+  $message .= $_POST["message"];
+
+  if(mb_send_mail($to,$subject,$message)) {
+    echo "メールが送信されました";
+  } else {
+    echo "メールの送信に失敗しました";
+  }
+} else {
+  echo "HTMLからのPOST送信受信に失敗しました";
+}
